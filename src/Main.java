129266a2f27f264b1e87df600ad7c00e2691b544
roisusi <65774200+roisusi@users.noreply.github.com>
2020-05-24 07:58:38 +0300

    tov
    rov

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:src/Main.java
 public class Main {
     public static void main (String[] Args){
         System.out.println("EZI");
@@ -6,12 +5,4 @@ public class Main {
         System.out.println("EZI");
     }
 }
-=======
-public class Main {
-    public static void main (String[] Args){
-        System.out.println("EZI");
-        System.out.println("EZI");
 
-    }
-}
->>>>>>> Stashed changes:Project/src/Main.java
